a1e3d88a0f0ddc714606b9f4f16fe400891effc0
Sherzel Smith <sherzelsmith@gmail.com>
2017-10-18 09:24:13 +0000

    Added data validations and error message

--- app/controllers/cvs_controller.rb
+++ app/controllers/cvs_controller.rb
@@ -1,13 +1,29 @@
 class CvsController < ApplicationController
   
   def index
+
   end
 
   def new
     @cv = Cv.new
   end
 
+  def create
+    @cv = Cv.create (cv_params)
+    if @cv.invalid?
+      flash[:error] = '<strong> Could not save. </strong> All fields are required.'
+    end
+    redirect_to root_path
+  end
+
   def about
+
+  end
+
+  private
+
+  def cv_params
+    params.require(:cv).permit(:company, :description, :dates)
   end
 
 end
--- app/models/cv.rb
+++ app/models/cv.rb
@@ -1,2 +1,5 @@
 class Cv < ApplicationRecord
+  validates :company, presence: true, length: {maximum: 140, minimum: 3}
+  validates :description, presence: true, length: {maximum: 500, minimum: 10}
+  validates :dates, presence: true
 end
